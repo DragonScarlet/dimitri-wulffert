9fbec6cd5ac550489ea1ce7fde33a9ecb8ce0515
D S <dimknight@gmail.com>
2022-11-20 22:19:37 +0100

    Refactored skills

--- src/app/experience/job-panel/project-panel/Project.ts
+++ src/app/experience/job-panel/project-panel/Project.ts
@@ -1,3 +1,5 @@
+import {Skill} from "../../../shared/skill";
+
 export class Project {
 
   constructor(public name: string, public time: string, public description: string) {
--- src/app/shared/skills.service.ts
+++ src/app/shared/skills.service.ts
@@ -1,5 +1,43 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Skill} from "./skill";
+
+// Programing Languages
+const JAVA = new Skill('Java', 'java', 'red');
+const PYTHON = new Skill('Python', 'python', 'yellow');
+const JAVASCRIPT = new Skill('JavaScript', 'javascript', 'yellow');
+const TYPESCRIPT = new Skill('TypeScript', 'javascript', 'blue');
+const HTML = new Skill('HTML', 'html', 'green');
+const CSS = new Skill('css', 'html', 'green');
+
+// FrameWorks
+const ANGULAR = new Skill('Angular', 'angular', 'red');
+const ANGULARJS = new Skill('AngularJs', 'angular', 'red');
+const PRIMEFACES = new Skill('Primefaces', 'robot', 'blue');
+const SPRING = new Skill('Spring', 'leaf', 'green');
+const HIBERNATE = new Skill('Hibernate', 'db', 'purple');
+const DOCKER = new Skill('Docker', 'docker', 'blue');
+const KUBERNETES = new Skill('Kubernetes', 'kubernetes', 'blue');
+const MOCKITO = new Skill('Mockito', 'mockito', 'purple');
+const CUCUMBER = new Skill('Cucumber', 'cucumber', 'green');
+const JUNIT = new Skill('JUnit', 'junit', 'blue');
+
+// DataBases
+const SQL = new Skill('SQL', 'db', 'purple');
+const POSTGRES = new Skill('Postgres', 'db', 'blue');
+const ORACLE = new Skill('ORACLE', 'db', 'red');
+const MYSQL = new Skill('MySQL', 'db', 'blue');
+const MONGO = new Skill('Mongo', 'db', 'yellow');
+const DB2 = new Skill('DB2', 'db', 'red');
+
+// Tools
+const GITKRAKEN = new Skill('GitKraken', 'gitkraken', 'green');
+const SOURCETREE = new Skill('SourceTree', 'jira', 'blue');
+const BITBUCKET = new Skill('BitBucket', 'jira', 'blue');
+const CONFLUENCE = new Skill('Confluence', 'jira', 'blue');
+const JIRA = new Skill('Jira', 'jira', 'blue');
+const INTELLIJ = new Skill('IntelliJ', 'jet', 'purple');
+const JENKINS = new Skill('Jenkins', 'jenkins', 'red');
+const SONARCUBE = new Skill('SonarCube', 'cube', 'purple');
 
 @Injectable({
   providedIn: 'root'
@@ -7,47 +45,22 @@ import {Skill} from "./skill";
 export class SkillsService {
 
   programingLanguages = [
-    new Skill('Java', 'java', 'red'),
-    new Skill('Python', 'python', 'yellow'),
-    new Skill('JavaScript', 'javascript', 'yellow'),
-    new Skill('TypeScript', 'javascript', 'blue'),
-    new Skill('HTML', 'html', 'green'),
-    new Skill('css', 'html', 'green'),
+    JAVA, PYTHON, JAVASCRIPT, TYPESCRIPT, HTML, CSS
   ];
   frameWorks = [
-    new Skill('Angular', 'angular', 'red'),
-    new Skill('AngularJs', 'angular', 'red'),
-    new Skill('Primefaces', 'robot', 'blue'),
-    new Skill('Spring', 'leaf', 'green'),
-    new Skill('Hibernate', 'db', 'purple'),
-    new Skill('Docker', 'docker', 'blue'),
-    new Skill('Kubernetes', 'kubernetes', 'blue'),
-    new Skill('Mockito', 'mockito', 'purple'),
-    new Skill('Cucumber', 'cucumber', 'green'),
-    new Skill('JUnit', 'junit', 'blue'),
+    ANGULAR, ANGULARJS, PRIMEFACES, SPRING, HIBERNATE, DOCKER, KUBERNETES, MOCKITO, CUCUMBER, JUNIT
   ];
 
   dataBases = [
-    new Skill('SQL', 'db', 'purple'),
-    new Skill('Postgres', 'db', 'blue'),
-    new Skill('ORACLE', 'db', 'red'),
-    new Skill('MySQL', 'db', 'blue'),
-    new Skill('Mongo', 'db', 'yellow'),
-    new Skill('DB2', 'db', 'red'),
+    SQL, POSTGRES, ORACLE, MYSQL, MONGO, DB2
   ];
 
   tools = [
-    new Skill('GitKraken', 'gitkraken', 'green'),
-    new Skill('SourceTree', 'jira', 'blue'),
-    new Skill('BitBucket', 'jira', 'blue'),
-    new Skill('Confluence', 'jira', 'blue'),
-    new Skill('Jira', 'jira', 'blue'),
-    new Skill('IntelliJ', 'jet', 'purple'),
-    new Skill('Jenkins', 'jenkins', 'red'),
-    new Skill('SonarCube', 'cube', 'purple'),
+    GITKRAKEN, SOURCETREE, BITBUCKET, CONFLUENCE, JIRA, INTELLIJ, JENKINS, SONARCUBE
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   getProgramingLanguages() {
     return this.programingLanguages;
@@ -65,7 +78,7 @@ export class SkillsService {
     return this.tools;
   }
 
-  getAllSkills(){
+  getAllSkills() {
     return [];
   }
 }
