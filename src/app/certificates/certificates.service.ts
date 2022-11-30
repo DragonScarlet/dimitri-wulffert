603201ff4c54e4a62e7642efc562169a4d4f064e
D S <dimknight@gmail.com>
2022-11-30 10:54:05 +0100

    Added video as background

--- src/app/certificates/certificates.service.ts
+++ src/app/certificates/certificates.service.ts
@@ -9,19 +9,19 @@ export class CertificatesService {
   certificates : Certificate[] = [
     new Certificate(
       'Docker',
-      'Docker & Kubernetes: The Practical Guide [2023 Edition]',
+      'Docker & Kubernetes: The Practical Guide',
       'assets/companies/docker.png',
       'https://www.udemy.com/course/docker-kubernetes-the-practical-guide/',
       'https://www.udemy.com/certificate/UC-ba382e33-e737-45ff-8204-15d852ee7776/'),
     new Certificate(
       'Kubernetes',
-      'Docker & Kubernetes: The Practical Guide [2023 Edition]',
+      'Docker & Kubernetes: The Practical Guide',
       'assets/companies/kubernetes.png',
       'https://www.udemy.com/course/docker-kubernetes-the-practical-guide/',
       'https://www.udemy.com/certificate/UC-ba382e33-e737-45ff-8204-15d852ee7776/'),
     new Certificate(
       'Angular',
-      'Angular - The Complete Guide (2022 Edition)',
+      'Angular - The Complete Guide',
       'assets/companies/angular.png',
       'https://www.udemy.com/course/the-complete-guide-to-angular-2/',
       'https://www.udemy.com/certificate/UC-96aadcb7-7992-4f40-9dc9-292784005c65/'),
