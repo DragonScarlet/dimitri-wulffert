63f767c09dfea8b73092137ce79d71055ae80246
Dimitri Wulffert <dimitri.wulffert@bbv.eu>
2026-01-15 10:43:45 +0100

    Apply consistent color scheme to skill categories
    - Programming Languages: red (core technologies)
    - Frameworks: green (frameworks building on languages)
    - Databases: purple (data storage technologies)
    - Cloud Services: blue (cloud/infrastructure services)
    - API Tools: orange (connectivity/integration tools)
    - General Tools: yellow (utility/support tools)
    - Testing Tools: purple (quality/validation tools)
    - Processes: black (methodology/standards)
    
    All skills within each category now share consistent colors for better visual organization.

--- src/app/shared/skills.service.ts
+++ src/app/shared/skills.service.ts
@@ -23,143 +23,143 @@ const black = 'black';
 
 // Programing Languages
 export const JAVA = new Skill('Java', 'java', red, PROGRAMMING_LANGUAGES, 3);
-export const PYTHON = new Skill('Python', 'python', yellow, PROGRAMMING_LANGUAGES, 3);
-export const JAVASCRIPT = new Skill('JavaScript', 'javascript', yellow, PROGRAMMING_LANGUAGES, 3);
-export const TYPESCRIPT = new Skill('TypeScript', 'javascript', blue, PROGRAMMING_LANGUAGES, 3);
-export const HTML = new Skill('HTML', 'html', green, PROGRAMMING_LANGUAGES, 1);
-export const CSS = new Skill('css', 'html', green, PROGRAMMING_LANGUAGES, 1);
-export const VB = new Skill('VB', 'cube', blue, PROGRAMMING_LANGUAGES, 1);
-export const CSHARP = new Skill('C#', 'cube', yellow, PROGRAMMING_LANGUAGES, 1);
+export const PYTHON = new Skill('Python', 'python', red, PROGRAMMING_LANGUAGES, 3);
+export const JAVASCRIPT = new Skill('JavaScript', 'javascript', red, PROGRAMMING_LANGUAGES, 3);
+export const TYPESCRIPT = new Skill('TypeScript', 'javascript', red, PROGRAMMING_LANGUAGES, 3);
+export const HTML = new Skill('HTML', 'html', red, PROGRAMMING_LANGUAGES, 1);
+export const CSS = new Skill('css', 'html', red, PROGRAMMING_LANGUAGES, 1);
+export const VB = new Skill('VB', 'cube', red, PROGRAMMING_LANGUAGES, 1);
+export const CSHARP = new Skill('C#', 'cube', red, PROGRAMMING_LANGUAGES, 1);
 export const CPLUS = new Skill('C++', 'cube', red, PROGRAMMING_LANGUAGES, 1);
-export const ANDROID = new Skill('Android', 'robot', green, PROGRAMMING_LANGUAGES, 1);
+export const ANDROID = new Skill('Android', 'robot', red, PROGRAMMING_LANGUAGES, 1);
 
 // FrameWorks
-export const ANGULAR = new Skill('Angular', 'angular', red, FRAMEWORKS, 3);
-export const ANGULARJS = new Skill('AngularJs', 'angular', red, FRAMEWORKS, 1);
-export const JQUERY = new Skill('JQUERY', 'javascript', blue, FRAMEWORKS, 1);
-export const JSF = new Skill('JSF', 'java', blue, FRAMEWORKS, 1);
-export const PRIMEFACES = new Skill('Primefaces', 'robot', blue, FRAMEWORKS, 1);
+export const ANGULAR = new Skill('Angular', 'angular', green, FRAMEWORKS, 3);
+export const ANGULARJS = new Skill('AngularJs', 'angular', green, FRAMEWORKS, 1);
+export const JQUERY = new Skill('JQUERY', 'javascript', green, FRAMEWORKS, 1);
+export const JSF = new Skill('JSF', 'java', green, FRAMEWORKS, 1);
+export const PRIMEFACES = new Skill('Primefaces', 'robot', green, FRAMEWORKS, 1);
 export const SPRING = new Skill('Spring', 'leaf', green, FRAMEWORKS,1);
-export const SPRINGBOOT = new Skill('Spring', 'leaf', green, FRAMEWORKS,1);
-export const HIBERNATE = new Skill('Hibernate', 'db', purple, FRAMEWORKS, 1);
+export const SPRINGBOOT = new Skill('SpringBoot', 'leaf', green, FRAMEWORKS,1);
+export const QUARKUS = new Skill('Quarkus', 'leaf', green, FRAMEWORKS,1);
+export const HIBERNATE = new Skill('Hibernate', 'db', green, FRAMEWORKS, 1);
 
 export const KUBERNETES = new Skill('Kubernetes', 'kubernetes', blue, CLOUD_SERVICES, 3);
 export const MOCKITO = new Skill('Mockito', 'mockito', purple, TESTING_TOOLS, 3);
-export const CUCUMBER = new Skill('Cucumber', 'cucumber', green, TESTING_TOOLS, 3);
-export const JUNIT = new Skill('JUnit', 'junit', blue, TESTING_TOOLS, 3);
+export const CUCUMBER = new Skill('Cucumber', 'cucumber', purple, TESTING_TOOLS, 3);
+export const JUNIT = new Skill('JUnit', 'junit', purple, TESTING_TOOLS, 3);
+export const CDI = new Skill('CDI', 'junit', purple, PROCESSES, 3);
 
 // DataBases
 export const SQL = new Skill('SQL', 'db', purple, DATABASES, 1);
-export const POSTGRES = new Skill('Postgres', 'db', blue, DATABASES, 1);
-export const ORACLE = new Skill('ORACLE', 'db', red, DATABASES, 1);
-export const MYSQL = new Skill('MySQL', 'db', blue, DATABASES, 1);
-export const MONGO = new Skill('Mongo', 'db', yellow, DATABASES, 3);
-export const DB2 = new Skill('DB2', 'db', red, DATABASES, 1);
+export const POSTGRES = new Skill('Postgres', 'db', purple, DATABASES, 1);
+export const ORACLE = new Skill('ORACLE', 'db', purple, DATABASES, 1);
+export const MYSQL = new Skill('MySQL', 'db', purple, DATABASES, 1);
+export const MONGO = new Skill('Mongo', 'db', purple, DATABASES, 3);
+export const DB2 = new Skill('DB2', 'db', purple, DATABASES, 1);
 
 // API Tools
-export const GITKRAKEN = new Skill('GitKraken', 'gitkraken', green, API_TOOLS, 3);
-export const SOURCETREE = new Skill('SourceTree', 'jira', blue, API_TOOLS, 1);
-export const BITBUCKET = new Skill('BitBucket', 'jira', blue, API_TOOLS, 1);
-export const CONFLUENCE = new Skill('Confluence', 'jira', blue, API_TOOLS, 1);
-export const JIRA = new Skill('Jira', 'jira', blue, API_TOOLS, 1);
-export const INTELLIJ = new Skill('IntelliJ', 'jet', purple, API_TOOLS, 3);
-export const JENKINS = new Skill('Jenkins', 'jenkins', red, API_TOOLS, 1);
-export const SONARCUBE = new Skill('SonarCube', 'cube', purple, API_TOOLS, 1);
-export const ECLIPSE = new Skill('Eclipse', 'cube', purple, API_TOOLS, 1);
-export const SVN = new Skill('SVN', 'cube', yellow, API_TOOLS, 1);
-export const GIT = new Skill('GIT', 'cube', purple, API_TOOLS, 3);
+export const GITKRAKEN = new Skill('GitKraken', 'gitkraken', orange, API_TOOLS, 3);
+export const SOURCETREE = new Skill('SourceTree', 'jira', orange, API_TOOLS, 1);
+export const BITBUCKET = new Skill('BitBucket', 'jira', orange, API_TOOLS, 1);
+export const CONFLUENCE = new Skill('Confluence', 'jira', orange, API_TOOLS, 1);
+export const JIRA = new Skill('Jira', 'jira', orange, API_TOOLS, 1);
+export const INTELLIJ = new Skill('IntelliJ', 'jet', orange, API_TOOLS, 3);
+export const JENKINS = new Skill('Jenkins', 'jenkins', orange, API_TOOLS, 1);
+export const SONARCUBE = new Skill('SonarCube', 'cube', orange, API_TOOLS, 1);
+export const ECLIPSE = new Skill('Eclipse', 'cube', orange, API_TOOLS, 1);
+export const SVN = new Skill('SVN', 'cube', orange, API_TOOLS, 1);
+export const GIT = new Skill('GIT', 'cube', orange, API_TOOLS, 3);
 
 // Processes
-export const SCRUM = new Skill('Scrum', 'cube', purple, PROCESSES, 1);
-export const KANBAN = new Skill('Kanban', 'cube', green, PROCESSES, 1);
-export const UFT = new Skill('UFT', 'cube', yellow, TESTING_TOOLS, 1);
+export const SCRUM = new Skill('Scrum', 'cube', black, PROCESSES, 1);
+export const KANBAN = new Skill('Kanban', 'cube', black, PROCESSES, 1);
+export const UFT = new Skill('UFT', 'cube', purple, TESTING_TOOLS, 1);
 export const ALM = new Skill('ALM', 'cube', yellow, GENERAL_TOOLS, 1);
-export const UNITY = new Skill('Unity', 'cube', purple, GENERAL_TOOLS, 1);
+export const UNITY = new Skill('Unity', 'cube', yellow, GENERAL_TOOLS, 1);
 
 // Cloud Services
 export const AWS = new Skill('AWS', 'cube', blue, CLOUD_SERVICES, 1);
 
 // General Tools
-export const AKHQ = new Skill('AKHQ', 'cube', blue, GENERAL_TOOLS, 1);
-export const ACTIVE_MQ = new Skill('ActiveMQ', 'cube', blue, GENERAL_TOOLS, 1);
-export const APACHE_TOMCAT = new Skill('Apache TomCat', 'cube', red, GENERAL_TOOLS, 1);
+export const AKHQ = new Skill('AKHQ', 'cube', yellow, GENERAL_TOOLS, 1);
+export const ACTIVE_MQ = new Skill('ActiveMQ', 'cube', yellow, GENERAL_TOOLS, 1);
+export const APACHE_TOMCAT = new Skill('Apache TomCat', 'cube', yellow, GENERAL_TOOLS, 1);
 
 // General Tools
 export const BASH = new Skill('Bash', 'cube', yellow, GENERAL_TOOLS, 1);
-export const BRUNO = new Skill('Bruno', 'cube', purple, GENERAL_TOOLS, 1);
-export const CAMUNDA = new Skill('Camunda', 'cube', blue, GENERAL_TOOLS, 1);
-export const CDI = new Skill('CDI', 'cube', blue, FRAMEWORKS, 1);
+export const BRUNO = new Skill('Bruno', 'cube', yellow, GENERAL_TOOLS, 1);
+export const CAMUNDA = new Skill('Camunda', 'cube', yellow, GENERAL_TOOLS, 1);
 export const CURL = new Skill('Curl', 'cube', yellow, GENERAL_TOOLS, 1);
-export const DBBEAVER = new Skill('DBBeaver', 'cube', blue, GENERAL_TOOLS, 1);
-export const DNS = new Skill('DNS', 'cube', blue, GENERAL_TOOLS, 1);
-export const DOCKER = new Skill('Docker', 'docker', blue, GENERAL_TOOLS, 3);
+export const DBBEAVER = new Skill('DBBeaver', 'cube', yellow, GENERAL_TOOLS, 1);
+export const DNS = new Skill('DNS', 'cube', yellow, GENERAL_TOOLS, 1);
+export const DOCKER = new Skill('Docker', 'docker', yellow, GENERAL_TOOLS, 3);
 export const ELASTIC = new Skill('Elastic', 'cube', yellow, GENERAL_TOOLS, 1);
 export const ELASTICSEARCH = new Skill('ElasticSearch', 'cube', yellow, GENERAL_TOOLS, 1);
-export const JBOSS = new Skill('JBoss', 'cube', red, GENERAL_TOOLS, 1);
-export const MAVEN = new Skill('Maven', 'cube', red, GENERAL_TOOLS, 1);
-export const NEXUS = new Skill('Nexus', 'cube', blue, GENERAL_TOOLS, 1);
-export const NPM = new Skill('npm', 'cube', red, GENERAL_TOOLS, 1);
-export const OPENSUSE = new Skill('OpenSUSE', 'cube', green, GENERAL_TOOLS, 1);
-export const QUARKUS = new Skill('Quarkus', 'cube', red, FRAMEWORKS, 1);
+export const JBOSS = new Skill('JBoss', 'cube', yellow, GENERAL_TOOLS, 1);
+export const MAVEN = new Skill('Maven', 'cube', yellow, GENERAL_TOOLS, 1);
+export const NEXUS = new Skill('Nexus', 'cube', yellow, GENERAL_TOOLS, 1);
+export const NPM = new Skill('npm', 'cube', yellow, GENERAL_TOOLS, 1);
+export const OPENSUSE = new Skill('OpenSUSE', 'cube', yellow, GENERAL_TOOLS, 1);
 export const SHELL = new Skill('Shell', 'cube', yellow, GENERAL_TOOLS, 1);
-export const WSL = new Skill('WSL', 'cube', blue, GENERAL_TOOLS, 1);
+export const WSL = new Skill('WSL', 'cube', yellow, GENERAL_TOOLS, 1);
 
 // Cloud Services
 export const CLOUDFLARE = new Skill('Cloudflare', 'cube', blue, CLOUD_SERVICES, 1);
 export const FLUXCD = new Skill('FluxCD', 'cube', blue, CLOUD_SERVICES, 1);
-export const OPENSHIFT = new Skill('Openshift', 'cube', red, CLOUD_SERVICES, 1);
+export const OPENSHIFT = new Skill('Openshift', 'cube', blue, CLOUD_SERVICES, 1);
 export const RANCHER = new Skill('Rancher', 'cube', blue, CLOUD_SERVICES, 1);
-export const TERRAFORM = new Skill('Terraform', 'cube', purple, CLOUD_SERVICES, 1);
+export const TERRAFORM = new Skill('Terraform', 'cube', blue, CLOUD_SERVICES, 1);
 
 // API Tools
-export const GITHUB = new Skill('GitHub', 'cube', black, API_TOOLS, 3);
-export const GITHUB_ACTIONS = new Skill('GitHub Actions', 'cube', black, API_TOOLS, 1);
-export const GITHUB_PAGES = new Skill('GitHub Pages', 'cube', black, API_TOOLS, 1);
+export const GITHUB = new Skill('GitHub', 'cube', orange, API_TOOLS, 3);
+export const GITHUB_ACTIONS = new Skill('GitHub Actions', 'cube', orange, API_TOOLS, 1);
+export const GITHUB_PAGES = new Skill('GitHub Pages', 'cube', orange, API_TOOLS, 1);
 export const GITLAB = new Skill('GitLab', 'cube', orange, API_TOOLS, 1);
 export const GRAFANA = new Skill('Grafana', 'cube', orange, API_TOOLS, 1);
-export const KIBANA = new Skill('Kibana', 'cube', red, API_TOOLS, 1);
-export const SONARQUBE = new Skill('Sonarqube', 'cube', green, API_TOOLS, 1);
+export const KIBANA = new Skill('Kibana', 'cube', orange, API_TOOLS, 1);
+export const SONARQUBE = new Skill('Sonarqube', 'cube', orange, API_TOOLS, 1);
 
 // Processes
-export const MICROSERVICES = new Skill('Microservices', 'cube', blue, PROCESSES, 1);
-export const OPENAPI = new Skill('OpenApi', 'cube', blue, PROCESSES, 1);
-export const REST = new Skill('REST', 'cube', blue, PROCESSES, 1);
-export const SOAP = new Skill('SOAP', 'cube', blue, PROCESSES, 1);
-export const SWAGGER = new Skill('Swagger', 'cube', green, PROCESSES, 1);
-export const UML = new Skill('UML', 'cube', blue, PROCESSES, 1);
-export const WEBOCKET = new Skill('Websphere', 'cube', blue, PROCESSES, 1);
-export const JMS = new Skill('JMS', 'cube', blue, PROCESSES, 1);
-export const JSON = new Skill('JSON', 'cube', yellow, PROCESSES, 1);
+export const MICROSERVICES = new Skill('Microservices', 'cube', black, PROCESSES, 1);
+export const OPENAPI = new Skill('OpenApi', 'cube', black, PROCESSES, 1);
+export const REST = new Skill('REST', 'cube', black, PROCESSES, 1);
+export const SOAP = new Skill('SOAP', 'cube', black, PROCESSES, 1);
+export const SWAGGER = new Skill('Swagger', 'cube', black, PROCESSES, 1);
+export const UML = new Skill('UML', 'cube', black, PROCESSES, 1);
+export const WEBOCKET = new Skill('Websphere', 'cube', black, PROCESSES, 1);
+export const JMS = new Skill('JMS', 'cube', black, PROCESSES, 1);
+export const JSON = new Skill('JSON', 'cube', black, PROCESSES, 1);
 
 // Testing Tools
 export const TESTCONTAINERS = new Skill('TestContainers', 'cube', purple, TESTING_TOOLS, 1);
 
 // Additional missing technologies
-export const KAFKA = new Skill('Kafka', 'cube', blue, FRAMEWORKS, 1);
-export const WILDFLY = new Skill('Wildfly', 'cube', red, GENERAL_TOOLS, 1);
+export const KAFKA = new Skill('Kafka', 'cube', green, FRAMEWORKS, 1);
+export const WILDFLY = new Skill('Wildfly', 'cube', yellow, GENERAL_TOOLS, 1);
 
 // Processes
-export const WSDL = new Skill('WSDL', 'cube', blue, PROCESSES, 1);
-export const XML = new Skill('XML', 'cube', yellow, PROCESSES, 1);
-export const YAML = new Skill('YAML', 'cube', yellow, PROCESSES, 1);
+export const WSDL = new Skill('WSDL', 'cube', black, PROCESSES, 1);
+export const XML = new Skill('XML', 'cube', black, PROCESSES, 1);
+export const YAML = new Skill('YAML', 'cube', black, PROCESSES, 1);
 
 // Additional missing technologies
-export const S3 = new Skill('S3', 'cube', orange, DATABASES, 1);
-export const MARIADB = new Skill('MariaDb', 'cube', blue, DATABASES, 1);
+export const S3 = new Skill('S3', 'cube', purple, DATABASES, 1);
+export const MARIADB = new Skill('MariaDb', 'cube', purple, DATABASES, 1);
 
 // Additional missing technologies
-export const J2EE = new Skill('J2EE', 'cube', blue, FRAMEWORKS, 1);
-export const EJB = new Skill('EJB', 'cube', blue, FRAMEWORKS, 1);
-export const JPA = new Skill('JPA', 'cube', blue, FRAMEWORKS, 1);
+export const J2EE = new Skill('J2EE', 'cube', green, FRAMEWORKS, 1);
+export const EJB = new Skill('EJB', 'cube', green, FRAMEWORKS, 1);
+export const JPA = new Skill('JPA', 'cube', green, FRAMEWORKS, 1);
 
 // General Tools
-export const NEOVIM = new Skill('NeoVim', 'jet', purple, GENERAL_TOOLS, 1);
-export const APACHE_CAMEL = new Skill('ApacheCamel', 'cube', blue, GENERAL_TOOLS, 1);
+export const NEOVIM = new Skill('NeoVim', 'jet', yellow, GENERAL_TOOLS, 1);
+export const APACHE_CAMEL = new Skill('ApacheCamel', 'cube', yellow, GENERAL_TOOLS, 1);
 
 // Processes
-export const SCRUM2 = new Skill('Scrum', 'cube', purple, PROCESSES, 1);
-export const WEBOCKET2 = new Skill('Websphere', 'cube', blue, PROCESSES, 1);
-export const BPMN = new Skill('BPMN', 'cube', blue, PROCESSES, 1);
+export const SCRUM2 = new Skill('Scrum', 'cube', black, PROCESSES, 1);
+export const WEBOCKET2 = new Skill('Websphere', 'cube', black, PROCESSES, 1);
+export const BPMN = new Skill('BPMN', 'cube', black, PROCESSES, 1);
 
 @Injectable({
   providedIn: 'root'
