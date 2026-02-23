3e6e9f0fc04e718ffe9ce15cd6f75df36898abf0
Dimitri Wulffert <dimitri.wulffert@bbv.eu>
2026-02-23 14:34:13 +0100

    Fix two page export

--- src/app/shared/pdf.service.ts
+++ src/app/shared/pdf.service.ts
@@ -2,21 +2,42 @@ import { Injectable } from '@angular/core';
 import html2pdf from 'html2pdf.js';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PdfService {
+  constructor() {}
 
-  constructor() { }
-
-  async generatePdfFromElement(element: HTMLElement, filename: string = 'CV.pdf'): Promise<void> {
+  async generatePdfFromElement(
+    element: HTMLElement,
+    filename: string = 'CV.pdf',
+  ): Promise<void> {
     const opt = {
       margin: 0,
       filename: filename,
-      image: { type: 'jpeg' as const, quality: 0.98 },
-      html2canvas: { scale: 2, useCORS: true, allowTaint: true, logging: false },
-      jsPDF: { unit: 'mm' as const, format: 'a4' as const, orientation: 'portrait' as const }
+      image: { type: 'jpeg' as const, quality: 1.0 },
+      html2canvas: {
+        scale: 2,
+        useCORS: true,
+        allowTaint: true,
+        logging: false,
+      },
+      jsPDF: {
+        unit: 'mm' as const,
+        format: 'a4' as const,
+        orientation: 'portrait' as const,
+      },
+      pagebreak: { mode: 'avoid-all' },
     };
 
-    await html2pdf().set(opt).from(element).save();
+    // Hacking the two page export
+    const worker = html2pdf().set(opt).from(element).toPdf();
+    const pdfInstance = await worker.get('pdf');
+    const pageCount = pdfInstance.internal.getNumberOfPages();
+    if (pageCount > 1) {
+      for (let i = pageCount; i > 1; i--) {
+        pdfInstance.deletePage(i);
+      }
+    }
+    await pdfInstance.save(filename);
   }
 }
